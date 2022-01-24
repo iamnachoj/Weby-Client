d8a40b85aa0acb42b2b08cf7723961d8a543dd1c
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2022-01-24 11:20:54 +0100

    revert last commit

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="jumbotron">
+      <h2>Home</h2>
+      <p className="lead">Welcome to Weby!</p>
+    </div>
+  )
+}
