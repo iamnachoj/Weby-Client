df5550bbf37290acacc3783c83835cbed3743faa
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2022-01-28 16:29:08 +0100

    add signout functionality

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,20 +1,38 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+
+function signout(next) {
+  if(typeof window !== undefined){
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    next();
+    return fetch("http://localhost:8080/signout", {
+      method: "GET"
+    })
+     .then(res => {
+       console.log(res.status)
+       return res.json()
+     })
+     .catch(err => console.log(err))
+  }
+}
 
 export default function Menu(){
+  const navigate = useNavigate("/")
   return (
     <nav className="navbar navbar-expand-lg navbar-light">
       <div className="container">
         <Link to="/" className="navbar-brand me-2"><h1 style={{fontFamily: "'Geo', sans-serif", fontSize: "3rem"}}>Weby</h1></Link>
 
         <div id="navbarButtonsExample">
-          <div class="d-flex align-items-center">
+          <div className="d-flex align-items-center">
             <Link to="/signin" className="btn btn-lg btn-link px-2 me-1">
               Login
             </Link>
             <Link to="/signup" className="btn btn-lg btn-link px-2 me-1">
               Sign up
             </Link>
+            <button onClick={() => {signout(() => {navigate("/")})}} className="btn btn-lg btn-link px-2 me-1">Sign out</button>
           </div>
         </div>
 
