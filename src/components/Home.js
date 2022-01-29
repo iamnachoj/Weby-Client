772422b364f83121c0f92748394f31eb4d99a306
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2022-01-29 18:22:04 +0100

    add some provisional content to Homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,23 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div className="jumbotron">
-      <h2 className="lead">Welcome to Weby!</h2>
+    <div className="container">
+      <h1 className="mt-5">Welcome to Weby!</h1>
+      <h2 className="mt-5">The Social Network</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum in mi eu ultrices. Maecenas in risus sit amet 
+      tellus sollicitudin maximus vulputate vel nisi. Fusce vel arcu porttitor, sagittis augue sit amet, ullamcorper turpis. Donec 
+      volutpat ornare lobortis. Nam a vulputate risus, eget pretium mauris. Maecenas pharetra at sapien nec sodales</p>
+      <p>Suspendisse id eleifend ante, sit amet consequat est. In efficitur, tortor eget cursus pharetra, lectus turpis ultrices orci,
+       eget tempor dui tortor ac mauris. Maecenas euismod, leo et consequat dapibus, felis augue rutrum quam, at vehicula lacus felis ac 
+       tellus. Cras aliquet, risus ut laoreet ultricies, augue turpis venenatis ipsum, sit amet maximus nulla dui nec dutaiaes</p>
+       <h2 className="mt-5">Faster, easier, and safer</h2>
+       <p>Maecenas euismod, leo et consequat dapibus, felis augue rutrum quam, consectetur adipiscing elit. Suspendisse bibendum in mi 
+       eu ultrices. Maecenas in risus sit amet tellus sollicitudin maximus vulputate vel nisi.</p>
+       <h2 className="mt-5">Keep friends, no reels</h2>
+       <p>Tate risus?, eget pretium mauris?... Maecenas pharetra at sapien nec sodales</p>
+      <p>Suspendisse id eleifend ante, sit amet consequat est. In efficitur, tortor eget cursus pharetra, lectus turpis ultrices orci,
+       eget tempor dui tortor ac mauris. Maecenas euismod Maecenas euismod, leo et consequat dapibus, felis augue rutrum quam, consectetur adipiscing elit. Suspendisse bibendum in mi 
+       eu ultrices. Maecenas in risus sit amet tellus sollicitudin maximus vulputate vel nisi.</p>
     </div>
   );
 }
