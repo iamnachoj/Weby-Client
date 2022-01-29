9c3cc96b9169e55f8b15ed5f5e2dfedd00a578c7
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2022-01-29 19:30:30 +0100

    add profile editing and deleting buttons

--- src/user/Profile.js
+++ src/user/Profile.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import {Link, useParams } from "react-router-dom";
 import {getUser} from './apiUser'
 import {Navigate} from "react-router-dom";
+import { isAuthenticated } from "../auth";
 
 export default function Profile(){
   const [user, setUser] = useState("");
   const [redirectToSignin, setRedirectToSignin] = useState(false)
   const {userId} = useParams()
+  const profile =  JSON.parse(localStorage.getItem("user"));
 
    // useEffect is a hook that takes the same job as ComponentDidMount in class components. 
    // this function will therefore apply this function as soon as the component mounts.
@@ -27,11 +29,27 @@ export default function Profile(){
 
     return (
       <>
-      <div className="container jumbotron mt-5">
-       <h2 className="mb-5">Profile</h2>
-       <p><b>Username: </b>{user.name}</p>
-       <p><b>Email: </b>{user.email}</p>  
-       <p><b>Joined: </b>{new Date(user.created).toDateString()}</p>
+      <div className="container">
+       <main className="jumbotron mt-5 row">
+        <div className="col-md-6">
+            <h2 className="mb-5">Profile</h2>
+            <p><b>Username: </b>{user.name}</p>
+            <p><b>Email: </b>{user.email}</p>  
+            <p><b>ID: </b>{user._id}</p>
+            <p><b>Joined: </b>{new Date(user.created).toDateString()}</p>
+        </div>
+        {isAuthenticated() && userId === profile._id && user.name === profile.name ? 
+          <div className="col-md-6">
+          <div className="d-flex flex-row-reverse">
+            <Link to={`/users/delete/${user._id}`} className="btn btn-sm btn-danger">Delete</Link>
+            <Link to={`/users/edit/${user._id}`} className="btn btn-sm">Edit</Link>
+          </div>
+          
+        </div>
+        :
+        <></>
+        }
+       </main>
       </div>
      </>
     )
