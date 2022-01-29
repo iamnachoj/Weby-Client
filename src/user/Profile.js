837cfaaa1fb6d48235ba6de377a3163a9c7e29e7
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2022-01-29 17:21:36 +0100

    get user using useEffect functional component equivalent to ComponentDidMount

--- src/user/Profile.js
+++ src/user/Profile.js
@@ -1,25 +1,47 @@
-import React from "react";
-import {Link} from "react-router-dom";
-import { isAuthenticated } from "../auth";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {Navigate} from "react-router-dom";
 
-export default function Profile() {
-  const user = JSON.parse(localStorage.getItem("user"))
-   
-    return(
-       <div className="container">
-       {isAuthenticated()
-        ?
-        <div className="jumbotron mt-4">
-          <h2 className="mb-5">Profile</h2>
-          <p>User name: {user.name}</p>
-          <p>Email: {user.email}</p>
-        </div>
-        :
-        <h2 className="alert alert-primary mt-5">
-           No user. Please, <Link to="/signin">Log in</Link>
-        </h2>
-       }
-         
-       </div>
+export default function Profile(){
+  const [user, setUser] = useState("");
+  const [redirectToSignin, setRedirectToSignin] = useState(false)
+  const {userId} = useParams()
+
+   // useEffect is a hook that takes the same job as ComponentDidMount in class components. 
+   // this function will therefore apply this function as soon as the component mounts.
+   useEffect(()=> {
+    function getUser(){
+      const token = localStorage.getItem("token")
+      return fetch(process.env.REACT_APP_API_URL + "/users/" + userId, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}` // sending this, I am authorized to get the user
+        }
+      })
+     .then((response) => {return response.json()})
+     .then(data => {
+       if(data.error){
+         setRedirectToSignin(true)
+       } else{
+         setUser(data)
+       } 
+     })
+    }
+     getUser()
+    }, []) // eslint-disable-line react-hooks/exhaustive-deps
+    
+   if(redirectToSignin){
+    return <Navigate to="/signin" />
+  }
+
+    return (
+      <>
+      <div className="container jumbotron mt-5">
+       <h2 className="mb-5">Profile</h2>
+       <p><b>Username: </b>{user.name}</p>
+       <p><b>Email: </b>{user.email}</p>  
+       <p><b>Joined: </b>{new Date(user.created).toDateString()}</p>
+      </div>
+     </>
     )
 }
