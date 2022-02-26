45063c28e5ab20907758e060e815c3eb7958a93e
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2022-02-27 00:29:56 +0100

    add size validation for avatar. 2mb max

--- src/user/editprofile.js
+++ src/user/editprofile.js
@@ -23,9 +23,11 @@ export default function EditProfile() {
    function handleChange(event) {
     let name = event.target.name;
     let value = name === "avatar" ? event.target.files[0] : event.target.value;
+    const fileSize = name === "avatar" ? event.target.files[0].size : 0;
     userData.set(name, value);
     setUser({
       ...user,
+      fileSize,
       error: "",
       [name]: value
     })
@@ -34,11 +36,20 @@ export default function EditProfile() {
    function clickSubmit(event) {
     event.preventDefault();
     setLoading(true)
+    if(user.fileSize > 200000){
+      setUser({
+        ...user,
+        error: "file is too big. maximum just 2 mb"
+      })
+      setLoading(false)
+      return null
+    }
     if(user.password !== user.confirmPassword){
       setUser({
         ...user,
         error: "passwords do not match"
       })
+      setLoading(false)
       return null
     } else {
     const token = localStorage.getItem("token")
@@ -86,7 +97,7 @@ export default function EditProfile() {
                   <label htmlFor="confirmPassword" className="text-muted">Confirm new password</label>
                   <input onChange={handleChange} value={user.confirmPassword || ""} name="confirmPassword" id="confirmPassword" type="password" className="form-control" />
                 </div>
-                {user.error ? <div className="alert alert-danger">passwords do not match</div> : null}
+                {user.error ? <div className="alert alert-danger">{user.error}</div> : null}
                 {loading ? <div className="alert alert-primary">Loading...</div>: null }
                 <button onClick={clickSubmit} className="btn btn-sm ml-0">Update</button>
                 <Link to={`/users/${user._id}`} className="btn btn-sm btn-primary">go back</Link>
