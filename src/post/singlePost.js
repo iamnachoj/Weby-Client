ddd0da412a29849effbc7cacfc41bb7e24063b4c
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2022-05-09 15:48:42 +0200

    add post structure to singlePost component

--- src/post/singlePost.js
+++ src/post/singlePost.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
-import {useParams } from "react-router-dom";
+import {Link, useParams } from "react-router-dom";
 import {getPost} from './apiPost'
 
 export default function SinglePost(){
     const [post, setPost] = useState({});
     const {postId} = useParams()
+    const userLink = post.postedBy ? "/users/" + post.postedBy._id : "/posts"
+    const photoUrl = post.photo ? `${process.env.REACT_APP_API_URL}/posts/photo/${post._id}` : null
     useEffect(()=> {
         getPost(postId)
         .then(data => {
@@ -16,11 +18,17 @@ export default function SinglePost(){
        })
        }, [postId])
     return (
-        <div>
-            <p>this post is {postId}</p>
-            {console.log(post)}
-            <p>{post.title}</p>
-            <p>{post.body}</p>
-        </div>
+      <div to="/" className="post-card mt-4 mb-2 p-3">
+        <h3 className="mb-3" style={{display: "block"}}>{post.title}</h3>
+        <p>{post.body}</p>
+        {photoUrl ? <img className="mb-3 img-thumbnail" style={{height: "200px", width: "auto"}} src={photoUrl} alt=""/> : null }
+        <div className="mark mb-2">
+            <p className="mr-2 font-italic small" style={{display: "inline"}}>Posted by: {" "}
+            <Link to={userLink}>{post.postedBy ? post.postedBy.name : " Unknown"}</Link> {" "}
+            on  {new Date(post.created).toDateString()} at {new Date(post.created).toLocaleTimeString()}
+            </p>
+         <br/>
+       </div>
+      </div>
     )
 }
