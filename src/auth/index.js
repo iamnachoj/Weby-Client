b68ef64a854a8f22e4e24cf9459925390fa617d8
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2022-06-08 16:11:23 +0200

    implement logic on socialLogin

--- src/auth/index.js
+++ src/auth/index.js
@@ -38,6 +38,14 @@ export function signout(next) {
      .catch(err => console.log(err))
   }
 }
+
+export const authenticate = (jwt, next) => {
+  if (typeof window !== 'undefined') {
+      localStorage.setItem('jwt', JSON.stringify(jwt));
+      next();
+  }
+};
+
 export function isAuthenticated(){
   if(typeof window == undefined){
     return false
@@ -78,4 +86,21 @@ export const resetPassword = resetInfo => {
           return response.json();
       })
       .catch(err => console.log("this is the error!", err));
+};
+
+export const socialLogin = user => {
+  return fetch(`${process.env.REACT_APP_API_URL}/social-login/`, {
+      method: "POST",
+      headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+      },
+      // credentials: "include", // works only in the same origin
+      body: JSON.stringify(user)
+  })
+      .then(response => {
+          console.log("signin response: ", response);
+          return response.json();
+      })
+      .catch(err => console.log(err));
 };
